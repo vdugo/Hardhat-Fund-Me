ca0c8ea5d709d013208916e870d2e1d3b1a542f4
Vincent <v3rdugo@yahoo.com>
2022-08-02 18:58:40 -0700

    add more configuration settings install dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox")
 require('hardhat-deploy')
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY 
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      // accounts: given by Hardhat
+      chainId: 31337,
+    }
+  },
   solidity: "0.8.8",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true
+  }
 };
