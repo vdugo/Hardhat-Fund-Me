426afc8a6daaa783ba28cfc79d64c603318f70aa
Vincent <v3rdugo@yahoo.com>
2022-08-04 14:34:05 -0700

    all contracts deploy

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,17 +1,31 @@
-const { networkConfig } = require('../helper-hardhat-config')
+const { networkConfig, developmentChains } = require('../helper-hardhat-config')
 const { network } = require('hardhat')
 
 module.exports = async ({ getNamedAccounts, deployments }) =>
 {
-    const { deploy, log } = deployments
+    const { deploy, log, get } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
 
-    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    let ethUsdPriceFeedAddress
+
+    if (developmentChains.includes(network.name))
+    {
+        const ethUsdAggregator = await get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    }
+    else
+    {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
 
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [address],
+        args: [ethUsdPriceFeedAddress],
         log: true
     })
+    log("FundMe deployed")
+    log("---------------------------------------------------")
 }
+
+module.exports.tags = ["all", "fundme"]
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,4 +1,7 @@
 const networkConfig = {
+    31337: {
+        name: "localhost"
+    },
     4: {
         name: "rinkeby",
         ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e"
