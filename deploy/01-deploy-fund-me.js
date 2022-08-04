9898ffc4300248f43366a39e84d042225fbf5372
Vincent <v3rdugo@yahoo.com>
2022-08-04 14:44:02 -0700

    automatic verification of contracts for Etherscan

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,5 +1,7 @@
 const { networkConfig, developmentChains } = require('../helper-hardhat-config')
 const { network } = require('hardhat')
+const { verify } = require('../utils/verify')
+require('dotenv').config()
 
 module.exports = async ({ getNamedAccounts, deployments }) =>
 {
@@ -19,12 +21,18 @@ module.exports = async ({ getNamedAccounts, deployments }) =>
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     }
 
+    const args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [ethUsdPriceFeedAddress],
+        args: args,
         log: true
     })
     log("FundMe deployed")
+    // if we're not on development chains, then automatically verify the code on Etherscan
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY)
+    {
+        await verify(fundMe.address, args)
+    }
     log("---------------------------------------------------")
 }
 
