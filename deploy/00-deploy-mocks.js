b4d18734d2482f52a19eb8fb2baf69fc10f5afc3
Vincent <v3rdugo@yahoo.com>
2022-08-04 13:41:16 -0700

    add Chainlink MockV3Aggregator.sol mock contract

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,8 @@
+const { network } = require('hardhat')
+
+module.exports = async ({ getNamedAccounts, deployments }) =>
+{
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+}
